1fb1cb9df26a1f9376feb9cabe8ed142f56735c3
EstebanCaponio <estebancaponio@gmail.com>
2025-06-26 14:22:05 +0200

    feat: snack1

---
+++ snack1/script.js
@@ -0,0 +1,16 @@
+// Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
+
+function getPostTitle(id) {
+    const promessa = new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(Response => Response.json())
+            .then(data => resolve(data))
+            .catch(reject);
+    });
+
+    return promessa;
+};
+
+getPostTitle(3)
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
