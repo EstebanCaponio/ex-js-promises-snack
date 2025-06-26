0b923966da7dd2b409badf8b3208861e6f213dab
EstebanCaponio <estebancaponio@gmail.com>
2025-06-26 17:04:12 +0200

    feat: snack1 bonus

---
+++ snack1-bonus/script.js
@@ -0,0 +1,27 @@
+// Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
+
+function getPost(id) {
+
+    return new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(Response => Response.json())
+            .then(data => {
+                fetch(`https://dummyjson.com/users/${data.userId}`)
+                    .then(intResp => intResp.json())
+                    .then(user => {
+                        const result = {
+                            ...data,
+                            user
+                        }
+                        resolve(result)
+                    })
+                    .catch(reject);
+
+            })
+            .catch(reject);
+    });
+};
+
+const test = getPost(3);
+
+console.log(test);
